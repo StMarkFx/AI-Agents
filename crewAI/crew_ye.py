c88324f84c45d96102e13566d7322f507057e65a
St. Mark <adebayomarkadedayo@gmail.com>
2025-01-21 00:20:33 +0100

    cleared

--- crewAI/crew_ye.py
+++
@@ -1,9 +0,0 @@
-eifhieofhewiofhjefuigfeufguefuefvu
-jrguidsuigdubshdbvsub
- def prompting_rag_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['prompting_rag_task'],
-            # tools=[rag_tool, query_instagram_db_tool],
-            context=[self.merge_results_task()],
-            # output_pydantic=ContentCreatorInfo,
-            output_file="rag_output.md"
